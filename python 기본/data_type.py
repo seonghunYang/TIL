0632c814d76ae15194159b3b7cf10adaee89a47b
seonghunYang <didtjdgns852@naver.com>
2020-07-10 02:35:21 +0900

    2장 연습문제

--- python 기본/data_type.py
+++ python 기본/data_type.py
@@ -180,3 +180,46 @@ a = 3
 b = 5
 a, b = b, a
 
+# 연습문제
+#1
+print((80 + 75 + 55) / 3)
+#2
+print(13 % 2)
+#3
+num = "881120-1068234"
+year = "19" + num[:2]
+print(year + num[2:6] + " " + num[7:])
+#4
+pin = "881120-1068234"
+print(pin[7])
+#5
+a = "a:b:c:d"
+print(a.replace(":", "#"))
+#6
+a = [1, 3, 5, 4, 2]
+a.sort()
+a.reverse()
+print(a)
+#7
+a = ['Life', 'is', 'too', 'short']
+a = " ".join(a)
+print(a)
+#8
+a = (1,2,3)
+print(a + (4,))
+#9
+#3 key에는 불변값만 올수 있음 list 못온다
+#10
+a = {'A':90, 'B':80, 'C':70}
+#dic도 pop 사용가능
+result = a.pop("B")
+print(a)
+print(result)
+#11
+a = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5]
+a = set(a)
+print(a)
+#12
+a = b = [1, 2, 3]
+a[1] = 4
+print(b, "= [1, 4, 3]")
