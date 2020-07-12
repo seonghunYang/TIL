9b99bb6ac866305460fd660956e78effff3683a5
seonghunYang <didtjdgns852@naver.com>
2020-07-12 11:36:51 +0900

    구조 변경

---
+++ algorithm/python 기본/built_in_fuction.py
@@ -0,0 +1,13 @@
+a = [1, -1, -3, 4]
+print(abs(-1))
+print(all([1, 2, 3]))
+print(any([1, 2, 3, 0]))
+print(chr(97))
+print()
+print(a)
+def positive(x):
+    return x > "a"
+a =  [1, -3, 2, 0, -5, 6]
+print(list(filter(positive,"absd")))
+print(a)
+print(max("ABS"))
